a63821569042caa4799e762a01cdd074a1541117
sajin <sajinrules@gmail.com>
2015-10-13 00:36:02 +0530

    deprecated React.render remove,used ReactDOM.render

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
 var App = require('./components/app');
 var React = require('react');
-React.render(<App/>,document.getElementById('main'));
+var ReactDOM = require('react-dom');
+ReactDOM.render(<App/>,document.getElementById('main'));
