eeaa3cb2c980782770c0c1d2e2b32f01cd16e320
sajin <sajinrules@gmail.com>
2015-10-14 01:06:23 +0530

    small changes

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,7 +1,16 @@
 var React = require('react');
+const RaisedButton = require('material-ui/lib/raised-button');
+const AppBar = require('material-ui/lib/app-bar');
+const IconButton = require('material-ui/lib/icon-button');
+//const IconMenu = require('material-ui/lib/icon-menu');
+//const NavigationClose = require('material-ui/lib/navigation-close');
 var App = React.createClass({
 	render : function(){
-		return <h1>My Flux App </h1>
+		return(
+			<AppBar
+				title="Title"
+				iconClassNameRight="muidocs-icon-navigation-expand-more" />
+		)
 	}
 });
 
